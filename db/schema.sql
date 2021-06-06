bda19da6d013f6d7756a3dbaade1300a9d2635e9
Eliadad <eliasashtonsaunders@gmail.com>
2021-06-06 16:22:50 -0700

    edit

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL (10,2),
+    wage DECIMAL (10,2),
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
